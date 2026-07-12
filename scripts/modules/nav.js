feedc362e19fcefff2762c230c465bd329506525
Tressa-Millering <Wickedracerfrog@gmail.com>
2026-07-12 13:45:43 -0500

    Implemented footer, fixed smaller bugs

--- scripts/modules/nav.js
+++ scripts/modules/nav.js
@@ -1,18 +1,23 @@
 export default function initNav() {
-    const nav = document.querySelector('nav');
+    const nav = document.querySelector('.navbar');
 
     let active = false;
+
+    const scrollButton = document.querySelector('.scroll-to-top');
 
     document.addEventListener('scroll', e => {
         if (window.scrollY >= 150 && !active) {
             active = true;
             nav.classList.add('scrolled');
+            nav.style.paddng = ""
+            scrollButton.style.opacity ='1';
             return;
         }
 
         if (window.scrollY < 150 && active) {
             active = false;
             nav.classList.remove('scrolled');
+            scrollButton.style.opacity = '0';
         }
     })
 
--- scripts/modules/scroll-buttons.js
+++ scripts/modules/scroll-buttons.js
@@ -3,6 +3,15 @@ export default function initScrollButtons() {
 
     const buttons = document.querySelectorAll('.scroll-link');
 
+    document.querySelector('.scroll-to-top').addEventListener('click', (e) => {
+        sections[0].scrollIntoView();
+    })
+
+    document.querySelector('.enter-button').addEventListener('click', (e) => {
+        sections[1].scrollIntoView();
+    });
+
+
     buttons.forEach((link, index) => {
         link.addEventListener('click', (e) => {
             sections[index].scrollIntoView();
