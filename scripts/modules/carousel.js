37b852ac3de95d3fdac47b26d3f54c2074e56fd9
Tressa-Millering <Wickedracerfrog@gmail.com>
2026-07-13 15:26:46 -0500

    Implemented Carousel

--- scripts/modules/carousel.js
+++ scripts/modules/carousel.js
@@ -1,16 +1,24 @@
 export default function initCarousel() {
     const carousels = document.querySelectorAll('.carousel');
-    const forwardButtons = document.querySelectorAll('.carousel-forward');
-    const backButtons = document.querySelectorAll('.carousel-backward');
+    const forwardButtons = document.querySelectorAll('.carousel-prev');
+    const backButtons = document.querySelectorAll('.carousel-next');
 
+    const length = carousels[0].children[0].childElementCount;
+    let currIndex = 0;
     let currPos = 0;
+
+
 
     forwardButtons.forEach((forwardButton, index) => {
         forwardButton.addEventListener('click', (e) => {
             const kids = carousels[index].children[0];
             currPos += kids.offsetWidth;
-
-            kids.style.transform = `translateX(${currPos}px)`;
+            currIndex++;
+            if (currIndex >= length) {
+                currIndex = 0;
+                currPos = 0;
+            }
+            kids.style.transform = `translateX(-${currPos}px)`;
         })
     })
 
@@ -18,8 +26,12 @@ export default function initCarousel() {
         backButton.addEventListener('click', (e) => {
             const kids = carousels[index].children[0];
             currPos -= kids.offsetWidth;
-
-            kids.style.transform = `translateX(${currPos}px)`;
+            currIndex--;
+            if (currIndex < 0) {
+                currIndex = length;
+                currPos = ((length-1) * kids.offsetWidth);
+            }
+            kids.style.transform = `translateX(-${currPos}px)`;
         })
     })
 
