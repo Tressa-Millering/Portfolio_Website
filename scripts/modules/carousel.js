b092c0e4d6704116e4e32465c8578a6d701324ec
Tressa-Millering <Wickedracerfrog@gmail.com>
2026-07-18 17:35:13 -0500

    Styled and implemented contact section

--- scripts/modules/carousel.js
+++ scripts/modules/carousel.js
@@ -7,32 +7,68 @@ export default function initCarousel() {
             content: carousel.children[0],
             currIndex: 0,
             currPos: 0,
-            width: carousel.children[0].offsetWidth,
             length: carousel.children[0].childElementCount,
             nextButton: carousel.querySelector('.carousel-next'),
-            prevButton: carousel.querySelector('.carousel-prev')
+            prevButton: carousel.querySelector('.carousel-prev'),
+            selectors: carousel.children[1].children[2].querySelectorAll('button')
         }
     })
 
+    window.addEventListener('resize', () => {
+        carousels.forEach(carousel => { updateWidths(carousel) })
+    })
+
+
     carousels.forEach((carousel, index) => {
         carousel.prevButton.addEventListener('click', () => {
-            carousels[index].currPos += carousels[index].width;
-            carousels[index].currIndex++;
-            if (carousels[index].currIndex >= carousels[index].length) {
-                carousels[index].currIndex = 0;
-                carousels[index].currPos = 0;
+            carousel.currIndex++;
+            if (carousel.currIndex >= carousel.length) {
+                carousel.currIndex = 0;
+                carousel.currPos = 0;
+            } else {
+                carousel.currPos = carousel.content.children[carousel.currIndex].offsetLeft;
             }
+            setSelectors(carousel, carousel.selectors[carousel.currIndex]);
+            carousel.content.style.transform = `translateX(-${carousel.currPos}px)`;
+        })
+
+        carousel.nextButton.addEventListener('click', () => {
+            carousel.currIndex--;
+            if (carousel.currIndex < 0) {
+                carousel.currIndex = carousel.length - 1;
+                carousel.currPos = getCurrentPosition(carousel, carousel.currIndex);
+            } else {
+                carousel.currPos = carousel.content.children[carousel.currIndex].offsetLeft;
+            }
+            setSelectors(carousel, carousel.selectors[carousel.currIndex])
             carousels[index].content.style.transform = `translateX(-${carousels[index].currPos}px)`;
         })
-        carousel.nextButton.addEventListener('click', () => {
-            carousels[index].currPos -= carousels[index].width;
-            carousels[index].currIndex--;
-            if (carousels[index].currIndex < 0) {
-                carousels[index].currIndex = carousels[index].length;
-                carousels[index].currPos = ((carousels[index].length - 1) * carousels[index].width);
-            }
-            carousels[index].content.style.transform = `translateX(-${carousels[index].currPos}px)`;
+
+        carousel.selectors.forEach((selector, index) => {
+            selector.addEventListener('click', () => {
+                setSelectors(carousel, selector)
+                carousel.currIndex = index;
+                carousel.currPos = carousel.content.children[index].offsetLeft;
+                carousel.content.style.transform = `translateX(-${carousel.currPos}px)`;
+
+            })
         })
     })
 
+    function setSelectors(carousel, selector) {
+        carousel.selectors.forEach((s) => { s.ariaSelected = 'false'; })
+        selector.ariaSelected = (selector.ariaSelected === 'true') ? 'false' : 'true';
+    }
+
+
+    function getCurrentPosition(carousel, index) {
+        return carousel.content.children[index].offsetLeft;
+    }
+
+    function updateWidths(carousel) {
+        carousel.currPos = getCurrentPosition(carousel, carousel.currIndex)
+        carousel.content.style.transform = `translateX(-${carousel.currPos}px)`
+    }
+
 }
+
--- scripts/modules/scroll-buttons.js
+++ scripts/modules/scroll-buttons.js
@@ -3,18 +3,22 @@ export default function initScrollButtons() {
 
     const buttons = document.querySelectorAll('.scroll-link');
 
-    document.querySelector('.scroll-to-top').addEventListener('click', (e) => {
+    document.querySelector('.scroll-to-top').addEventListener('click', () => {
         sections[0].scrollIntoView();
     })
 
-    document.querySelector('.enter-button').addEventListener('click', (e) => {
+    document.querySelector('.enter-button').addEventListener('click', () => {
         sections[1].scrollIntoView();
     });
 
 
     buttons.forEach((link, index) => {
-        link.addEventListener('click', (e) => {
+        link.addEventListener('click', () => {
             sections[index].scrollIntoView();
         })
+    })
+
+    document.getElementById('footer-about').addEventListener('click', () => {
+        sections[1].scrollIntoView();
     })
 }
