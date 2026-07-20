a235982577482e58d701c401c70d26525d706e07
Tressa-Millering <Wickedracerfrog@gmail.com>
2026-07-20 13:19:14 -0500

    Updated background image link

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,8 @@ import initCardLinks from './modules/card-links.js';
 import initScrollIntoView from './modules/scroll-into-view.js';
 import initCarousel from './modules/carousel.js';
 
+console.log("Script Loaded!")
+
 document.addEventListener('DOMContentLoaded', () => {
     initNav();
     initModals();
